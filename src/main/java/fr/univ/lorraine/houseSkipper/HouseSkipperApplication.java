1d402777d5b29184867915d7a7272ddda3d7bcb9
Simon HAJEK <simonhajek88@gmail.com>
2018-11-16 14:59:50 +0100

    INIT springboot

---
+++ src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
@@ -0,0 +1,12 @@
+package fr.univ.lorraine.houseSkipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSkipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseSkipperApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.univ.lorraine.houseSkipper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HouseSkipperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
