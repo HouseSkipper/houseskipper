d15a26e992c2189fea6de113e28db677431fb8ec
Simon HAJEK <simonhajek88@gmail.com>
2018-11-30 17:05:01 +0100

    Setup JWT back

--- src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
@@ -2,9 +2,16 @@ package fr.univ.lorraine.houseSkipper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HouseSkipperApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseSkipperApplication.class, args);
