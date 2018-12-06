ac02e876084c1a1340e085cfe0492a7ac9d822e2
Simon HAJEK <simonhajek88@gmail.com>
2018-12-06 17:47:53 +0100

    When register create skills

--- src/main/java/fr/univ/lorraine/houseSkipper/controller/UserController.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/controller/UserController.java
@@ -3,6 +3,8 @@ package fr.univ.lorraine.houseSkipper.controller;
 
 import fr.univ.lorraine.houseSkipper.exceptions.UserEmailAlreadyExists;
 import fr.univ.lorraine.houseSkipper.model.ApplicationUser;
+import fr.univ.lorraine.houseSkipper.model.Skill;
+import fr.univ.lorraine.houseSkipper.repositories.SkillRepository;
 import fr.univ.lorraine.houseSkipper.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,12 @@ public class UserController {
 
     private UserRepository UserRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private SkillRepository skillRepository;
 
-    public UserController(UserRepository UserRepository,
+    public UserController(UserRepository UserRepository, SkillRepository skillRepository,
                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.UserRepository = UserRepository;
+        this.skillRepository = skillRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
@@ -27,6 +31,9 @@ public class UserController {
         try {
             System.out.println(applicationUser.toString());
             UserRepository.save(applicationUser);
+            skillRepository.save(new Skill("Jardinage", 0, applicationUser));
+            skillRepository.save(new Skill("Plomberie", 0, applicationUser));
+
         } catch(Exception e) {
             throw new UserEmailAlreadyExists();
         }
