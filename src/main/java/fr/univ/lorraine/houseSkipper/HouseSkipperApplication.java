5a120d0e58511b9c20a2a5ab688dc4c88d9b9852
Allae Ddine Lemtiri <ddine.allae@gmail.com>
2018-12-07 18:57:55 +0100

    Tasks Finished

--- src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
@@ -1,18 +1,9 @@
 package fr.univ.lorraine.houseSkipper;
 
-import fr.univ.lorraine.houseSkipper.model.Task;
-import fr.univ.lorraine.houseSkipper.repositories.HouseRepository;
-import fr.univ.lorraine.houseSkipper.repositories.RoomRepository;
-import fr.univ.lorraine.houseSkipper.repositories.TaskRepository;
-import fr.univ.lorraine.houseSkipper.repositories.UserRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.Date;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class HouseSkipperApplication {
@@ -24,28 +15,6 @@ public class HouseSkipperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseSkipperApplication.class, args);
-	}
-
-	@Bean
-	ApplicationRunner init(TaskRepository taskRepository, RoomRepository roomRepository, HouseRepository houseRepository, UserRepository userRepository) {
-
-		roomRepository.deleteAll();
-		houseRepository.deleteAll();
-		taskRepository.deleteAll();
-		userRepository.deleteAll();
-
-		return args -> {
-			Stream.of(
-					new Task("X1", "X1","X1", new Date(), "En Attente"),
-					new Task("X2", "X2","X2", new Date(), "En Attente"),
-					new Task("X3", "X3","X3", new Date(), "En Attente"),
-					new Task("X4", "X4","X4", new Date(), "En Attente")
-			).forEach(task -> {
-				taskRepository.save(task);
-			});
-			// Query database
-			taskRepository.findAll().forEach(System.out::println);
-		};
 	}
 
 }
--- src/main/java/fr/univ/lorraine/houseSkipper/controller/RoomController.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/controller/RoomController.java
@@ -1,28 +1,47 @@
 package fr.univ.lorraine.houseSkipper.controller;
 
+import fr.univ.lorraine.houseSkipper.model.ApplicationUser;
+import fr.univ.lorraine.houseSkipper.model.House;
 import fr.univ.lorraine.houseSkipper.model.Room;
 import fr.univ.lorraine.houseSkipper.repositories.RoomRepository;
+import fr.univ.lorraine.houseSkipper.service.AuthenticatedUserService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class RoomController {
 
     private RoomRepository repository;
+    private AuthenticatedUserService authenticatedUserService;
 
-    public RoomController(RoomRepository repository){
+    public RoomController(RoomRepository repository, AuthenticatedUserService authenticatedUserService){
         this.repository = repository;
+        this.authenticatedUserService = authenticatedUserService;
     }
 
     @GetMapping("/rooms")
     @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Room> roomList(){
-        return repository.findAll().stream().collect(Collectors.toList());
+        ApplicationUser user = this.authenticatedUserService.getAuthenticatedUser();
+        List<House> houses = user.getHouses();
+        List<Room> rooms = new ArrayList<>();
+        for (House h: houses
+             ) {
+            rooms.addAll(h.getRooms());
+
+        }
+        for (Room r: rooms
+             ) {
+            System.out.println(r.getRoomName()+"---------------------------------------");
+        }
+        return rooms.stream().collect(Collectors.toList());
     }
 
     @GetMapping("/{houseName}/rooms")
