68359b4ef6401600cbd4c4b4dfc80b76583a2537
Simon HAJEK <simonhajek88@gmail.com>
2018-12-07 18:48:16 +0100

    Fix Init

--- src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/HouseSkipperApplication.java
@@ -23,28 +23,6 @@ public class HouseSkipperApplication {
 		SpringApplication.run(HouseSkipperApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner init(TaskRepository taskRepository, RoomRepository roomRepository, HouseRepository houseRepository, UserRepository userRepository, SkillRepository skillRepository) {
 
-		roomRepository.deleteAll();
-		houseRepository.deleteAll();
-		taskRepository.deleteAll();
-		userRepository.deleteAll();
-		skillRepository.deleteAll();
-
-
-		return args -> {
-			Stream.of(
-					new Task("X1", "X1","X1", new Date(), "En Attente"),
-					new Task("X2", "X2","X2", new Date(), "En Attente"),
-					new Task("X3", "X3","X3", new Date(), "En Attente"),
-					new Task("X4", "X4","X4", new Date(), "En Attente")
-			).forEach(task -> {
-				taskRepository.save(task);
-			});
-			// Query database
-			taskRepository.findAll().forEach(System.out::println);
-		};
-	}
 
 }
