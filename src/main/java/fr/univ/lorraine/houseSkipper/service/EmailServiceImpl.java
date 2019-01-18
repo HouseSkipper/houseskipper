a90984606a4e65afe59ed447090986f00fe27289
Simon HAJEK <simonhajek88@gmail.com>
2019-01-18 20:09:01 +0100

    Refactor EmailService

--- src/main/java/fr/univ/lorraine/houseSkipper/service/EmailServiceImpl.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/service/EmailServiceImpl.java
@@ -4,8 +4,6 @@ import fr.univ.lorraine.houseSkipper.model.ApplicationUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -27,64 +25,31 @@ public class EmailServiceImpl{
         this.javaMailSender = emailSender;
     }
 
-    public void sendEmail() throws MailException {
-
-        /*
-         * This JavaMailSender Interface is used to send Mail in Spring Boot. This
-         * JavaMailSender extends the MailSender Interface which contains send()
-         * function. SimpleMailMessage Object is required because send() function uses
-         * object of SimpleMailMessage as a Parameter
-         */
-        // EXAMPLE
-        /*SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo("simonhajek88@gmail.com");
-        mail.setFrom("platform.houseskipper@gmail.com");
-        mail.setSubject("Testing Mail API");
-        mail.setText("Hurray ! You have done that dude...");
-        */
-        /*
-         * This send() contains an Object of SimpleMailMessage as an Parameter
-         */
-        //javaMailSender.send(mail);
-    }
-
-    public void sendConfirmationEmail(ApplicationUser user) throws MessagingException {
+    private void sendHtmlEmail(String subject, String htmlContent, String toEmail) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         boolean multipart = true;
 
         MimeMessageHelper helper = new MimeMessageHelper(message, multipart, "utf-8");
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
 
-        helper.setTo(user.getUsername());
-        helper.setSubject("Bienvenue sur Houseskipper");
+        message.setContent(htmlContent, "text/html");
+        javaMailSender.send(message);
+    }
 
-
+    public void sendConfirmationEmail(ApplicationUser user) throws MessagingException {
         String url = env.getProperty("client.url")+"/#/validateAccount/";
         String html = "<html>Bonjour "+user.getFirstname()+"\n"+"Confirmez-votre inscription en cliquant sur le lien suivant : <a href='"+url+user.getEmailToken()+"'>Ici</a></html>";
 
-
-        message.setContent(html, "text/html");
-
-        javaMailSender.send(message);
+        sendHtmlEmail("Bienvenue sur Houseskipper", html, user.getUsername());
     }
 
     // TODO Ne pas oublier de générer un nouveau emailToken et de le repasser à invalid
     public void sendChangeConfirmationEmail(ApplicationUser user) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        boolean multipart = true;
-
-        MimeMessageHelper helper = new MimeMessageHelper(message, multipart, "utf-8");
-
-        helper.setTo(user.getUsername());
-        helper.setSubject("Changement d'adresse email");
-
-
         String url = env.getProperty("client.url")+"/#/validateAccount/";
         String html = "<html>Bonjour "+user.getFirstname()+"\n"+"Veuillez confirmer votre nouvelle adresse email en cliquant : <a href='"+url+user.getEmailToken()+"'>Ici</a></html>";
 
-
-        message.setContent(html, "text/html");
-
-        javaMailSender.send(message);
+        sendHtmlEmail("Changement d'adresse email", html, user.getUsername());
     }
 
 }
