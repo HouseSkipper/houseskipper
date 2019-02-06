112b2387f0be3b2c693bf62873c808bae81bac67
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-02-06 17:41:20 +0100

    Petit modif message d'erreur

--- src/main/java/fr/univ/lorraine/houseSkipper/exceptions/InvalidValidationTokenException.java
+++ src/main/java/fr/univ/lorraine/houseSkipper/exceptions/InvalidValidationTokenException.java
@@ -3,7 +3,7 @@ package fr.univ.lorraine.houseSkipper.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.CONFLICT, reason="This validation email token does not exists")  // 409
+@ResponseStatus(value= HttpStatus.CONFLICT, reason="Code de vérification inconnu")  // 409
 public class InvalidValidationTokenException extends RuntimeException {
 
     public InvalidValidationTokenException() {
